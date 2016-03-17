06529c5d7b2da7fc8daf1ac16897bd9efc3f6ebb
Master <634665383@qq.com>
2016-03-17 22:46:38 +0800

    16.28

---
+++ 16.28/16.28/median.cpp
@@ -0,0 +1,19 @@
+/*template <typename T>
+bool median(const vector<T> &c, T &m)
+{
+	vector<T> temp(c);
+	if (temp.size()%2==0)
+	{
+		return false;
+	}
+	sort(temp.begin(), temp.end());
+
+	vector<T>::size_type index = temp.size() / 2;
+	if (temp[index] > temp[index - 1] && temp[index] < temp[index + 1])
+	{
+		return true;
+	}
+	else
+		return false;
+}
+*/
